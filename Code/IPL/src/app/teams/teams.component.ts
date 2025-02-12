55db5dc9454b73d8039e354272de4b77552e376c
Praveen Badatya <pbpraveenbadatya@gmail.com>
2025-02-12 15:34:42 +0530

    new app

---
+++ Code/IPL/src/app/teams/teams.component.ts
@@ -0,0 +1,12 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-teams',
+  standalone: false,
+  templateUrl: './teams.component.html',
+  styleUrl: './teams.component.css'
+})
+export class TeamsComponent {
+   @Input() teamLogo = '';
+    @Input() teamName = '';
+}
